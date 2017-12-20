89e492bb35a93889ecf9c3aa3ac5471ae85f0db8
Simon Bull <simon.bull.84@googlemail.com>
2017-12-20 11:30:03 +0000

    Check config when registering handlers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 const { union } = require('lodash');
 const invite = require('./handlers/invite');
 
+const validateConfig = (config) => {
+  if (!config) {
+    throw new Error('Must supply config');
+  }
+
+  if (!config.migrationAdmin) {
+    throw new Error('Must supply migrationAdmin config');
+  }
+
+  if (!config.migrationAdmin.directories) {
+    throw new Error('Must supply migrationAdmin.directories config');
+  }
+
+  if (!config.migrationAdmin.organisations) {
+    throw new Error('Must supply migrationAdmin.organisations config');
+  }
+};
+
 const register = (config, logger) => {
+  validateConfig(config);
+
   const inviteHandlers = invite.register(config, logger);
 
   return union(inviteHandlers);
